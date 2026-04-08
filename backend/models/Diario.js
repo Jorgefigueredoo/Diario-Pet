34452e4feae63b2f07bebf9348d4a26c848f8cbb
Jorgefigueredoo <jorgeafigueredo@gmail.com>
2026-04-08 11:46:49 -0300

    back end em node js

---
+++ backend/models/Diario.js
@@ -0,0 +1,10 @@
+const moongoose = require('mongoose');
+
+const DiarioSchema = new moongoose.Schema(
+    {
+        titulo : {type: String},
+        descricao : {type: String},
+    }
+)
+
+module.exports = moongoose.model('Diario', DiarioSchema);
---
+++ backend/server.js
@@ -0,0 +1,17 @@
+const dns = require("dns");
+dns.setServers(["8.8.8.8", "8.8.4.4"]);
+
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+require("dotenv").config();
+
+const app = express();
+app.use(express.json());
+app.use(cors("/api/diario", require("./routes/diarioRoutes")));
+
+mongoose.connect(process.env.MONGO_URI).then(() =>
+  app.listen(process.env.PORT, () => {
+    console.log(`Servidor rodando !!!`);
+  }),
+);
