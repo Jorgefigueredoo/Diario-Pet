dede4241f602fa6b01b7ef3db1d1111c3e7b66a1
Jorgefigueredoo <jorgeafigueredo@gmail.com>
2026-04-15 10:05:51 -0300

    alterações no back

---
+++ backend/models/Produto.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+
+const ProdutoSchema = new mongoose.Schema(
+    {
+        nome: { type: String },
+        preco: { type: Number },
+        descricao: { type: String },
+    }
+)
+
+module.exports = mongoose.model('Produto', ProdutoSchema);
--- backend/server.js
+++ backend/server.js
@@ -10,6 +10,7 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 app.use("/api/diario", require("./routes/diarioRoutes"));
+app.use("/api/produto", require("./routes/produtoRoutes"));
 
 mongoose.connect(process.env.MONGO_URI).then(() =>
   app.listen(process.env.PORT, () => {
