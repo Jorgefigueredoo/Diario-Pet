2c080b56cb3e26bf1233bbf2623c41c898122449
Jorgefigueredoo <jorgeafigueredo@gmail.com>
2026-04-15 09:05:58 -0300

    exportação da função

--- backend/routes/diarioRoutes.js
+++ backend/routes/diarioRoutes.js
@@ -8,3 +8,5 @@ router.post('/diarios', diarioController.createDiario);
 router.put('/diarios/:id', diarioController.updateDiario);
 router.delete('/diarios/:id', diarioController.deleteDiario);
 router.get('/diarios/:id', diarioController.umDiario);
+
+module.exports = router;
